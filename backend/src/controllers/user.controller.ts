db004b0580bc0199dcbce40d59d3f339e1473616
Mohamed Isse <mohamedr201@gmail.com>
2025-03-23 02:05:36 -0400

    Initial setup for backend

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response, Router } from 'express';
+
+
+class UserController{
+    public path = '/user'
+    public router = Router();
+
+    constructor(){
+        this.initalizeRoutes()
+    }
+
+    initalizeRoutes(): void {
+        this.router.post(`new`, this.createUser);
+        this.router.get('/', this.getUserByEmail)
+        this.router.post('/edit/:userId', this.editUser)
+    }
+
+    private getUserByEmail(){
+
+    }
+
+    private editUser(){
+
+    }
+
+    private createUser(){
+        
+    }
+}
+
+export default UserController;
---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import App from './index';
+import UserController from './controllers/user.controller';
+import dotenv from 'dotenv';
+
+const port = process.env.PORT || '3000';
+
+const application = new App([
+    new UserController()
+])
+
+export default application.listen(port);
